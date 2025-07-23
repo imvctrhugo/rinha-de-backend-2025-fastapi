f02358485a3e470e97a10199498a134175bec2da
coactive-victor <victor@coactive.ai>
2025-07-23 13:38:01 -0300

    feat: add purge payments endpoint

--- api/app/routes/payments.py
+++ api/app/routes/payments.py
@@ -5,11 +5,11 @@ from ..config import settings
 from ..models.payments import ProcessPaymentRequest
 from ..shared.dependency import get_redis
 
-router = APIRouter(prefix="/payments")
+router = APIRouter()
 
 
 @router.post(
-    path="",
+    path="/payments",
     status_code=status.HTTP_202_ACCEPTED,
     summary="(Asynchronously) Process a payment request",
 )
@@ -19,4 +19,11 @@ async def process_payment(
 ) -> None:
     await redis.rpush(settings.REDIS_QUEUE, request.model_dump_json())
 
-    return
+
+@router.post(
+    path="/purge-payments",
+)
+async def purge_payments(
+    redis: Redis = Depends(get_redis),
+) -> None:
+    await redis.delete(settings.REDIS_PAYMENTS_KEY)
